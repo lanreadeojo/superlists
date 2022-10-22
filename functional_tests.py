87555bda0549caee0e3e31e9ff84143cf9c1023d
Lanre Adeojo <lanreadeojo03@gmail.com>
2022-10-22 15:16:31 -0400

    add "enter item into to do list" unit tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVistorTest(unittest.TestCase):
@@ -10,15 +12,28 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Lanre has heard about a cool new online to-do app.
         #She goes to check out its homepage
-        self.browser.get('http://127.0.0.1:8000/')
         #she notices the page title and header mention to-do lists
+        self.browser.get('http://127.0.0.1:8000/')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
 # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 #She types "Buy peacock feathers" into a text box(Lanre's hobby is
 # tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
 #When she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as item in a to-do list
@@ -26,7 +41,7 @@ class NewVistorTest(unittest.TestCase):
 
 #There is still a text box inviting her to add another item. She
 #enters "Use peacock feathers to make a fly" (Lanre is very methodical)
-
+        self.fail('Finish the test!')
 #The page updates again, and now shows both items on her list
 
 
