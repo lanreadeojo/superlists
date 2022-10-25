4e0993e163e2ddd46a3cad1f079ed32653c17b5f
Lanre Adeojo <lanreadeojo03@gmail.com>
2022-10-24 22:40:03 -0400

    improve funtional tests for table data

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVistorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Lanre has heard about a cool new online to-do app.
         #She goes to check out its homepage
@@ -28,29 +34,31 @@ class NewVistorTest(unittest.TestCase):
 #She types "Buy peacock feathers" into a text box(Lanre's hobby is
 # tying fly-fishing lures)
         inputbox.send_keys("Buy peacock feathers")
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 #When she hits enter, the page updates, and now the page lists
 # "1: Buy peacock feathers" as item in a to-do list
-
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
 #There is still a text box inviting her to add another item. She
 #enters "Use peacock feathers to make a fly" (Lanre is very methodical)
-        self.fail('Finish the test!')
-#The page updates again, and now shows both items on her list
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+#The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("Use peacock feathers to make a fly")
 
 #Lanre wonders whether the site will remember her list. Then she sees
 #that the site has generated a unique URL for her -- there is some
 #explanatory text to that effect
 
 #She visits that URL - her to-do list is still there.
-
+        self.fail('Finish the test!')
 
 #Satisfied, she goes back to sleep
 
