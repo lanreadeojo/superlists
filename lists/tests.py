6a4f2b44d1992e8c14bf7ac25f45e9cd10541bac
Lanre Adeojo <lanreadeojo03@gmail.com>
2022-11-15 23:31:18 -0500

    Add URL for creating new list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -16,16 +16,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
 
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"],'/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
@@ -52,13 +43,14 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        Item.objects.create(text = 'itemey 1')
-        Item.objects.create(text = 'itemey 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
 
-        self.assertContains(response,'itemey 1')
-        self.assertContains(response, 'itemey 2')
-
-
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
